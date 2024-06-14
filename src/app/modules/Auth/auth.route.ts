fcb2ccfac8fe78f69d9ca0ad3b5a08b0983af062
Shabbir8788 <hasanalshabbir257@gmail.com>
2024-06-14 23:01:27 +0600

    Updated AuthRoutes

--- src/app/modules/Auth/auth.route.ts
+++ src/app/modules/Auth/auth.route.ts
@@ -1,43 +1,9 @@
 import express from 'express'
-// import auth from '../../middleware/auth'
 import validateRequest from '../../middleware/validateRequest'
 import { AuthValidations } from './auth.validation'
 import { AuthControllers } from './auth.controller'
-// import { USER_ROLE } from '../user/user.constant'
-// import { AuthControllers } from './auth.controller'
-// import { AuthValidation, AuthValidations } from './auth.validation'
 
 const router = express.Router()
-
-// router.post(
-//   '/login',
-//   validateRequest(AuthValidation.loginValidationSchema),
-//   AuthControllers.loginUser,
-// )
-
-// router.post('/change-password', (req, res, next) => {
-//   auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student)(
-//     req,
-//     res,
-//     (err) => {
-//       if (err) return next(err)
-//       validateRequest(AuthValidation.changePasswordValidationSchema)(
-//         req,
-//         res,
-//         (err) => {
-//           if (err) return next(err)
-//           AuthControllers.changePassword(req, res, next)
-//         },
-//       )
-//     },
-//   )
-// })
-
-// router.post(
-//   '/refresh-token',
-//   validateRequest(AuthValidation.refreshTokenValidationSchema),
-//   AuthControllers.refreshToken,
-// )
 
 router.post(
   '/signup',
