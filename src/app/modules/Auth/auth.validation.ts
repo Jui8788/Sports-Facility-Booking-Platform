ed7e870003bbd0a7675f884c9a89c1fd1e838e88
Shabbir8788 <hasanalshabbir257@gmail.com>
2024-06-14 23:21:19 +0600

    Updated AuthRoutes

--- src/app/modules/Auth/auth.validation.ts
+++ src/app/modules/Auth/auth.validation.ts
@@ -1,29 +1,33 @@
 import { z } from 'zod'
 
 const signUpSchema = z.object({
-  name: z.string().min(1, { message: 'Name is required' }).trim(),
-  email: z
-    .string()
-    .email({ message: 'Invalid email format' })
-    .min(1, { message: 'Email is required' })
-    .trim(),
-  password: z
-    .string()
-    .min(6, { message: 'Password must be at least 6 characters long' }),
-  phone: z.string().min(1, { message: 'Phone number is required' }).trim(),
-  role: z.enum(['admin', 'user']),
-  address: z.string().min(1, { message: 'Address is required' }).trim(),
+  body: z.object({
+    name: z.string().min(1, { message: 'Name is required' }).trim(),
+    email: z
+      .string()
+      .email({ message: 'Invalid email format' })
+      .min(1, { message: 'Email is required' })
+      .trim(),
+    password: z
+      .string()
+      .min(6, { message: 'Password must be at least 6 characters long' }),
+    phone: z.string().min(1, { message: 'Phone number is required' }).trim(),
+    role: z.enum(['admin', 'user']),
+    address: z.string().min(1, { message: 'Address is required' }).trim(),
+  }),
 })
 
 const loginSchema = z.object({
-  email: z
-    .string()
-    .email({ message: 'Invalid email format' })
-    .min(1, { message: 'Email is required' })
-    .trim(),
-  password: z
-    .string()
-    .min(6, { message: 'Password must be at least 6 characters long' }),
+  body: z.object({
+    email: z
+      .string()
+      .email({ message: 'Invalid email format' })
+      .min(1, { message: 'Email is required' })
+      .trim(),
+    password: z
+      .string()
+      .min(6, { message: 'Password must be at least 6 characters long' }),
+  }),
 })
 
 export const AuthValidations = {
