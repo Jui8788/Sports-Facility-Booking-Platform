dd9f4e798562227a0a5077896ca07a52dc169c06
Shabbir8788 <hasanalshabbir257@gmail.com>
2024-07-02 02:34:55 +0600

    fixing deployment issue

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,3 @@
-// import express, { Application, Request, Response } from 'express'
-// import cors from 'cors'
-// import globalErrorHandler from './app/middleware/globalErrorHandler'
-// import NotFound from './app/middleware/notFound'
-// import cookieParser from 'cookie-parser'
-// import router from './app/routes'
-// import { BookingControllers } from './app/modules/Booking/booking.controller'
-
-// const app: Application = express()
-
-// // parsers
-// app.use(express.json())
-// app.use(cookieParser())
-// app.use(cors({ origin: ['http://localhost:5173'] }))
-
-// // application routes
-// app.use('/api', router)
-
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Hello World!')
-// })
-
-// app.get('/api/check-availability', BookingControllers.checkAvailability)
-
-// // Not Found Route
-// app.use(NotFound)
-
-// // globalErrorHandler
-// app.use(globalErrorHandler)
-
-// export default app
-
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import globalErrorHandler from './app/middleware/globalErrorHandler'
@@ -40,26 +8,24 @@ import { BookingControllers } from './app/modules/Booking/booking.controller'
 
 const app: Application = express()
 
-// Middleware
+// parsers
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ origin: ['http://localhost:5173'] }))
 
-// Routes
+// application routes
 app.use('/api', router)
 
-// Example route
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-// Route using BookingControllers
 app.get('/api/check-availability', BookingControllers.checkAvailability)
 
 // Not Found Route
 app.use(NotFound)
 
-// Global Error Handler
+// globalErrorHandler
 app.use(globalErrorHandler)
 
 export default app
