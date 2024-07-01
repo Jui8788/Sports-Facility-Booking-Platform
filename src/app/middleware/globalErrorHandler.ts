9f9934d66b6effb933adadbe915f0a9e78759efc
Shabbir8788 <hasanalshabbir257@gmail.com>
2024-07-02 02:01:22 +0600

    fixing deployment issue

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,6 +1,78 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+// import { ErrorRequestHandler } from 'express'
+// import { ZodError } from 'zod'
+// import { TErrorSource } from '../interface/error'
+// import handleZodError from '../errors/handleZodError'
+// import handleValidationError from '../errors/handleValidationError'
+// import handleCastError from '../errors/handleCastError'
+// import handleDuplicateError from '../errors/handleDuplicateError'
+// import AppError from '../errors/AppError'
+// import config from '../config/config'
+
+// const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+//   // setting default values
+//   let statusCode = 500
+//   let message = 'Something went wrong'
+
+//   let errorSources: TErrorSource = [
+//     {
+//       path: '',
+//       message: 'Something went wrong',
+//     },
+//   ]
+
+//   if (error instanceof ZodError) {
+//     const simplifiedError = handleZodError(error)
+//     statusCode = simplifiedError?.statusCode
+//     message = simplifiedError?.message
+//     errorSources = simplifiedError?.errorSources
+//   } else if (error?.name === 'ValidationError') {
+//     const simplifiedError = handleValidationError(error)
+//     statusCode = simplifiedError?.statusCode
+//     message = simplifiedError?.message
+//     errorSources = simplifiedError?.errorSources
+//   } else if (error?.name === 'CastError') {
+//     const simplifiedError = handleCastError(error)
+//     statusCode = simplifiedError?.statusCode
+//     message = simplifiedError?.message
+//     errorSources = simplifiedError?.errorSources
+//   } else if (error?.code === 11000) {
+//     const simplifiedError = handleDuplicateError(error)
+//     statusCode = simplifiedError?.statusCode
+//     message = simplifiedError?.message
+//     errorSources = simplifiedError?.errorSources
+//   } else if (error instanceof AppError) {
+//     statusCode = error?.statusCode
+//     message = error?.message
+//     errorSources = [
+//       {
+//         path: '',
+//         message: error?.message,
+//       },
+//     ]
+//   } else if (error instanceof Error) {
+//     message = error?.message
+//     errorSources = [
+//       {
+//         path: '',
+//         message: error?.message,
+//       },
+//     ]
+//   }
+
+//   // ultimate return
+//   return res.status(statusCode).json({
+//     success: false,
+//     message,
+//     errorSources,
+//     stack: config.NODE_ENV === 'development' ? error?.stack : null,
+//   })
+// }
+
+// export default globalErrorHandler
+
 import { ErrorRequestHandler } from 'express'
 import { ZodError } from 'zod'
 import { TErrorSource } from '../interface/error'
@@ -11,7 +83,13 @@ import handleDuplicateError from '../errors/handleDuplicateError'
 import AppError from '../errors/AppError'
 import config from '../config/config'
 
-const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+const globalErrorHandler: ErrorRequestHandler = (
+  error,
+  req,
+  res,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+  next
+) => {
   // setting default values
   let statusCode = 500
   let message = 'Something went wrong'
@@ -63,23 +141,12 @@ const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
   }
 
   // ultimate return
-  // return res.status(statusCode).json({
-  //   success: false,
-  //   message,
-  //   errorSources,
-  //   stack: config.NODE_ENV === 'development' ? error?.stack : null,
-  // })
-  if (!res.headersSent) {
-    res.status(statusCode).json({
-      success: false,
-      message,
-      errorSources,
-      stack: config.NODE_ENV === 'development' ? error?.stack : null,
-    })
-  } else {
-    // Handle case where headers were already sent
-    console.error('Headers were already sent. Cannot send error response.')
-  }
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    stack: config.NODE_ENV === 'development' ? error?.stack : null,
+  })
 }
 
 export default globalErrorHandler
